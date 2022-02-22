5b5c7d3cb68c630da1d086433fc44a08c563811a
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-02-22 23:36:51 +0300

    first commit

---
+++ resources/js/Pages/Modules/Customer/Index.vue
@@ -0,0 +1,46 @@
+<template>
+    <app-layout title="Müşteriler">
+        <Table :dataSource="customers" :columns="columns" />
+    </app-layout>
+</template>
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { Table } from "ant-design-vue"
+
+export default ({
+    name: 'CustomerIndex',
+    components: {
+        AppLayout,
+        Table
+    },
+    props: {
+        customers: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        }
+    },
+    setup() {
+        const columns = [
+            {
+                title: 'Müşteri',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email',
+            },
+            {
+                title: 'Telefon',
+                dataIndex: 'phone',
+                key: 'phone',
+            },
+        ]
+
+        return { columns }
+    }
+})
+</script>
