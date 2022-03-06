487ad5adef71abfc7499ca15d4b0bcd32bfd686e
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-03-07 00:03:30 +0300

    adds tasks page

---
+++ resources/js/Pages/Modules/Task/Index.vue
@@ -0,0 +1,44 @@
+<template>
+    <app-layout title="İş Emirleri">
+        <template #header>
+            İş Emirleri
+        </template>
+
+        <!--Add New Customer-->
+        <template #action-buttons>
+            selam
+        </template>
+        <Table :dataSource="tasks" :columns="columns" />
+    </app-layout>
+</template>
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { Table } from "ant-design-vue"
+
+export default ({
+    name: 'TaskIndex',
+    components: {
+        AppLayout,
+        Table
+    },
+    props: {
+        customers: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        }
+    },
+    setup() {
+        const columns = [
+            {
+                title: 'İş Emri ID',
+                dataIndex: 'code',
+                key: 'code',
+            }
+        ]
+
+        return { columns }
+    }
+})
+</script>
